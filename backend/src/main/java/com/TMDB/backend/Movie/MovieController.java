56f9925d51a8d0e2648660211dd2e975d5641a9c
Jayden, Gyeongmin Kim <gmkim716@gmail.com>
2024-08-14 18:44:03 +0900

    Movie List 컨트롤러를 완성한다
    1. upcoming
    2. top_rated
    3. popular
    4. now_playing

--- backend/src/main/java/com/TMDB/backend/Movie/MovieController.java
+++ backend/src/main/java/com/TMDB/backend/Movie/MovieController.java
@@ -20,17 +20,47 @@ public class MovieController {
   private final MovieService movieService;
   private final ObjectMapper objectMapper;
 
-  @GetMapping("now_playing")
+  @GetMapping("/list/now_playing")
   public ResponseEntity<?> getNowPlaying() {
     try {
       Response movieResponse = movieService.getNowPlayingMovies();  // 영화 데이터 가져오기
-      if (movieResponse != null) {
-        String jsonString = movieResponse.body() != null ? movieResponse.body().string() : null;
-        MovieResponseDto responseDto = objectMapper.readValue(jsonString, MovieResponseDto.class);  // 가져온 데이터를 MovieResponseDto로 변환
-        return ResponseEntity.ok(responseDto);
-      }
+      MovieResponseDto responseDto = objectMapper.readValue(movieResponse.body() != null ? movieResponse.body().string() : null, MovieResponseDto.class);  // 가져온 데이터를 MovieResponseDto로 변환
+      return ResponseEntity.ok(responseDto);
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      return ResponseEntity.badRequest().body("Now Playing 영화 정보를 가져오는 데 실패했습니다.");
+    }
+  }
+
+  @GetMapping("/list/popular")
+  public ResponseEntity<?> getPopular() {
+    try {
+      Response movieResponse = movieService.getPopularMovies();  // 영화 데이터 가져오기
+      MovieResponseDto responseDto = objectMapper.readValue(movieResponse.body() != null ? movieResponse.body().string() : null, MovieResponseDto.class);  // 가져온 데이터를 MovieResponseDto로 변환
+      return ResponseEntity.ok(responseDto);
+    } catch (IOException e) {
+      return ResponseEntity.badRequest().body("Popular 영화 정보를 가져오는 데 실패했습니다.");
+    }
+  }
+
+  @GetMapping("/list/top_rated")
+  public ResponseEntity<?> getTopRated() {
+    try {
+      Response movieResponse = movieService.getTopRatedMovies();  // 영화 데이터 가져오기
+      MovieResponseDto responseDto = objectMapper.readValue(movieResponse.body() != null ? movieResponse.body().string() : null, MovieResponseDto.class);  // 가져온 데이터를 MovieResponseDto로 변환
+      return ResponseEntity.ok(responseDto);
+    } catch (IOException e) {
+      return ResponseEntity.badRequest().body("Top Rated 영화 정보를 가져오는 데 실패했습니다.");
+    }
+  }
+
+  @GetMapping("/list/upcoming")
+  public ResponseEntity<?> getUpcoming() {
+    try {
+      Response movieResponse = movieService.getUpcomingMovies();  // 영화 데이터 가져오기
+      MovieResponseDto responseDto = objectMapper.readValue(movieResponse.body() != null ? movieResponse.body().string() : null, MovieResponseDto.class);  // 가져온 데이터를 MovieResponseDto로 변환
+      return ResponseEntity.ok(responseDto);
+    } catch (IOException e) {
+      return ResponseEntity.badRequest().body("Upcoming 영화 정보를 가져오는 데 실패했습니다.");
     }
   }
 }
--- backend/src/main/java/com/TMDB/backend/Movie/MovieService.java
+++ backend/src/main/java/com/TMDB/backend/Movie/MovieService.java
@@ -14,11 +14,16 @@ public class MovieService {
   @Value("${TMDB_API_TOKEN}")
   private String apiToken;
 
+  @Value("${LANGUAGE}")
+  private String language;
+
   private final OkHttpClient client = new OkHttpClient();
 
   public Response getNowPlayingMovies() throws IOException {
+
+    String url = "https://api.themoviedb.org/3/movie/now_playing?language=" + language + "&page=1";
     Request request = new Request.Builder()
-      .url("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1")
+      .url(url)
       .get()
       .addHeader("accept", "application/json")
       .addHeader("Authorization", "Bearer " + apiToken)
@@ -27,4 +32,36 @@ public class MovieService {
     return client.newCall(request).execute();
   }
 
+  public Response getPopularMovies() throws IOException {
+    Request request = new Request.Builder()
+      .url("https://api.themoviedb.org/3/movie/popular?language=" + language + "&page=1")
+      .get()
+      .addHeader("accept", "application/json")
+      .addHeader("Authorization", "Bearer " + apiToken)
+      .build();
+
+    return client.newCall(request).execute();
+  }
+
+  public Response getTopRatedMovies() throws IOException {
+    Request request = new Request.Builder()
+      .url("https://api.themoviedb.org/3/movie/top_rated?language=" + language + "&page=1")
+      .get()
+      .addHeader("accept", "application/json")
+      .addHeader("Authorization", "Bearer " + apiToken)
+      .build();
+
+    return client.newCall(request).execute();
+  }
+
+  public Response getUpcomingMovies() throws IOException {
+    Request request = new Request.Builder()
+      .url("https://api.themoviedb.org/3/movie/upcoming?language=" + language + "&page=1")
+      .get()
+      .addHeader("accept", "application/json")
+      .addHeader("Authorization", "Bearer " + apiToken)
+      .build();
+
+    return client.newCall(request).execute();
+  }
 }
